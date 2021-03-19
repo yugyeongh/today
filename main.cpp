23ec7e626eb907a7a992f005c818f04db9aa5034
noeyh <72396865+yugyeongh@users.noreply.github.com>
2021-03-20 02:34:13 +0900

    B10773-stack

--- main.cpp
+++ main.cpp
@@ -4,32 +4,21 @@
 using namespace std;
 
 int main() {
-    stack <int> s;
-    int n,a;
+    int n,sum=0,a;
     cin >> n;
     
-    string cmd;
+    stack <int> stack;
     for (int i=0;i<n;i++){
-        cin >> cmd;
-        
-        if (cmd=="push"){
-            cin >> a;
-            s.push(a);
-        } else if (cmd=="pop"){
-            if (s.empty()) cout << -1 << '\n';
-            else {
-                cout << s.top() << "\n";
-                s.pop();
-            }
-        } else if (cmd=="size"){
-            cout << s.size() << '\n';
-        } else if (cmd=="empty"){
-            if (s.empty()) cout << 1 <<'\n';
-            else cout << 0 << '\n';
-        } else if (cmd=="top"){
-            if (s.empty()) cout << -1 << '\n';
-            else cout << s.top() << '\n';
+        cin >> a;
+        if (a==0){
+            int t = stack.top();
+            sum -= t;
+            stack.pop();
+        } else {
+            stack.push(a);
+            sum+=a;
         }
     }
+    cout << sum << '\n';
     return 0;
 }
